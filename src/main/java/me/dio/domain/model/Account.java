b28bd6f44904333c83d614a2d52d1b462ceb4edf
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-08-01 12:32:36 -0300

    hotfix precision e scale

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -8,13 +8,16 @@ import java.math.BigDecimal;
 public class Account {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Column(unique = true)
     private String number;
+
     private String agency;
-    @Column(scale = 13, precision = 2)
+
+    @Column(precision = 13, scale = 2)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", scale = 13, precision = 2)
+    @Column(name = "additional_limit", precision  = 13, scale = 2)
     private BigDecimal limit;
 
     public Long getId() {
