2793f686b7b8ac7edeb98c57f37aed420c0bc95e
devCarlosAlexandre <carloalexandresousa@gmail.com>
2023-08-01 16:33:21 -0300

    Primeira tentativa de deploy na  Railway

--- src/main/java/me/dio/domain/repository/UserRepository.java
+++ src/main/java/me/dio/domain/repository/UserRepository.java
@@ -6,5 +6,5 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existByAccountNumber(String account);
+    boolean existsByAccountNumber(String account);
 }
